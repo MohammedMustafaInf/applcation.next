8727941b58b83d7e22c7622b30bb4f2612cf6c5c
welecomtopython <johndoe@example.com>
2024-05-18 02:25:18 +0300

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+// // next.config.js
+// module.exports = {
+//   reactStrictMode: true,
+//   // Other custom configurations
+// };
+// next.config.mjs
+const config = {
+  /* your configuration options here */
+};
+
+export default config;
